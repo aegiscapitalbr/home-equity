51ff34a2ff5289f9f2fc2add38bcb6b3ca8e22e6
esdrasfyy <fernaando.esdras@gmail.com>
2025-05-05 19:16:37 -0300

    fix: ids random

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 export let config = {
-  ids: [23538825, 23359206],
+  ids: [41, 43],
   currentIndex: 0,
   pipedrive_token: "d621629b52e52c2a7c4585cf46e2a2ce849a9058",
   bitrix_webhook_url: "https://b24-c2d8dr.bitrix24.com.br/rest/1/g63gyfl4a58wnfbn",
 };
 export const getNextUserId = () => {
   const userId = config.ids[config.currentIndex % config.ids.length];
-  config.currentIndex++; // Atualiza o índice para alternar no próximo uso
+  config.currentIndex++;
   return userId;
 };
